8d99a6830d1fb5a1041382efad1548e466de0bd4
GetStartHealth <getsdar39@gmail.com>
2025-07-03 04:47:47 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,9 +1,6 @@
 from openai import OpenAI
 from openai import AsyncOpenAI
 
-TOKEN_DEEP_SEEK = (
-   "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"
-)
 client = AsyncOpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=TOKEN_DEEP_SEEK,
