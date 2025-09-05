a39cbbd452e863f362ffa1c96fc1141c827a17a2
GetStartHealth <getsdar39@gmail.com>
2025-09-06 07:04:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'music-player-cache-v1';
+const CACHE_NAME = 'music-player-cache-v2';
 const urlsToCache = [
 
   "https://nextjs-boilerplate-i6pd.vercel.app/Русская душа - Богеме Ивлеевой (5).mp3",
